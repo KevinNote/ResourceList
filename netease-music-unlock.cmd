f8f9b216737062eb43a01353ac97c63f33815b81
Kevin CHEN <33132228+KevinZonda@users.noreply.github.com>
2021-02-10 16:38:17 +0000

    Create netease-music-unlock.cmd

---
+++ netease-music-unlock.cmd
@@ -0,0 +1,8 @@
+REM 网易云中国版权歌解锁
+REM 需要管理员权限
+REM REFER: https://github.com/fengjueming/unblock-NetEaseMusic
+REM REFER: https://github.com/fengjueming/unblock-NetEaseMusic/issues/42#issuecomment-596242788
+
+echo 158.199.142.239 music.163.com >> C:\WINDOWS\System32\drivers\etc\hosts
+echo 158.199.142.239 interface.music.163.com >> C:\WINDOWS\System32\drivers\etc\hosts
+echo 127.0.0.1 music.httpdns.c.163.com >> C:\WINDOWS\System32\drivers\etc\hosts
